92d2d63d376b7a23e89810cf1c53217656a21aa3
Leguna <ahmadtuflihunxd@gmail.com>
2021-05-31 17:40:08 +0700

    add notes initial

--- src/notes.js
+++ src/notes.js
@@ -1,3 +1,15 @@
-const notes = []
+const notes = [
+  {
+    title: 'Catatan A',
+    tags: [
+      'Android',
+      'Web'
+    ],
+    body: 'Isi dari catatan A',
+    id: 'aMSmQFVEsCqQi1qs',
+    createdAt: '2021-05-31T08:29:13.291Z',
+    updatedAt: '2021-05-31T08:29:13.291Z'
+  }
+]
 
 module.exports = notes
